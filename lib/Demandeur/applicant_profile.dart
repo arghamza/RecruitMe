1754465396000d0a0c6d4f5911a68fef5f7b2cf9
DaddaFarouk <faroukdadda@gmail.com>
2022-06-01 16:59:09 +0100

    responsive user profile

--- lib/Demandeur/applicant_profile.dart
+++ lib/Demandeur/applicant_profile.dart
@@ -32,116 +32,120 @@ class _ApplicantProfileState extends State<ApplicantProfile> {
                 textAlign: TextAlign.center,
                 style: GoogleFonts.montserrat(
                   textStyle: const TextStyle(
-                      fontSize: 35, fontWeight: FontWeight.bold),
+                      fontSize: 30, fontWeight: FontWeight.bold),
                 )),
-            Center(
-              child: Container(
-                decoration: BoxDecoration(
-                    border:
-                        Border.all(color: const Color(0xff35ddaa), width: 5),
-                    borderRadius: BorderRadius.circular(30)),
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            backgroundImage: widget.user.img == ""
-                                ? const AssetImage("images/avatar.png")
-                                : Image.network(
-                                    widget.user.img.toString(),
-                                  ).image,
-                            radius: 100,
-                          ),
-                          const SizedBox(
-                            height: 20.0,
-                          ),
-                          Expanded(
-                            flex: 0,
-                            child: Text(
+            const SizedBox(
+              height: 15.0,
+            ),
+            Flexible(
+              child: Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                      border:
+                          Border.all(color: const Color(0xff35ddaa), width: 5),
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: widget.user.img == ""
+                                  ? const AssetImage("images/avatar.png")
+                                  : Image.network(
+                                      widget.user.img.toString(),
+                                    ).image,
+                              minRadius: 30,
+                              maxRadius: 60,
+                            ),
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
                               '${widget.user.FirstName} ${widget.user.SecondName}',
                               style: GoogleFonts.montserrat(
                                   textStyle: const TextStyle(
                                       color: Colors.black,
                                       letterSpacing: .5,
-                                      fontSize: 30),
+                                      fontSize: 25),
                                   fontWeight: FontWeight.w600),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 20.0,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20.0),
-                            child: Column(
-                              children: [
-                                RichTextLine(
-                                  text: "${widget.user.email}",
-                                  title: 'Email :  ',
-                                ),
-                                const SizedBox(
-                                  height: 20.0,
-                                ),
-                                RichTextLine(
-                                  text: widget.user.details!["expYears"]
-                                      .toString(),
-                                  title: 'Années d\'expérience :  ',
-                                ),
-                                const SizedBox(
-                                  height: 20.0,
-                                ),
-                                RichTextLine(
-                                  text: "${widget.user.details!["domaine"]}",
-                                  title: 'Domaine :  ',
-                                ),
-                                const SizedBox(
-                                  height: 20.0,
-                                ),
-                                RichTextLine(
-                                  text: "${widget.user.details!["poste"]}",
-                                  title: 'Poste actuel :  ',
-                                ),
-                                const SizedBox(
-                                  height: 20.0,
-                                ),
-                                RichTextLine(
-                                  text: getCompetences(
-                                      widget.user.details!["competences"]),
-                                  title: 'Compétences :  ',
-                                ),
-                                TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).push(
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  ApplicantInfo(
-                                                    entreprise: widget.user
-                                                        .details!["entreprise"],
-                                                    domaine: widget.user
-                                                        .details!["domaine"],
-                                                    poste: widget
-                                                        .user.details!["poste"],
-                                                    linkedin: widget.user
-                                                        .details!["linkedin"],
-                                                    expYears: widget.user
-                                                        .details!["expYears"]
-                                                        .toString(),
-                                                    competence:
-                                                        widget.user.details![
-                                                            "competences"],
-                                                    url: widget.user.img!,
-                                                  )));
-                                    },
-                                    child: const Icon(
-                                      Icons.edit,
-                                      color: Color(0xff35ddaa),
-                                      size: 50,
-                                    ))
-                              ],
+                            const SizedBox(
+                              height: 20.0,
                             ),
-                          ),
-                        ],
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10.0),
+                              child: Column(
+                                children: [
+                                  RichTextLine(
+                                    text: "${widget.user.email}",
+                                    title: 'Email :  ',
+                                  ),
+                                  const SizedBox(
+                                    height: 15.0,
+                                  ),
+                                  RichTextLine(
+                                    text: widget.user.details!["expYears"]
+                                        .toString(),
+                                    title: 'Années d\'expérience :  ',
+                                  ),
+                                  const SizedBox(
+                                    height: 15.0,
+                                  ),
+                                  RichTextLine(
+                                    text: "${widget.user.details!["domaine"]}",
+                                    title: 'Domaine :  ',
+                                  ),
+                                  const SizedBox(
+                                    height: 15.0,
+                                  ),
+                                  RichTextLine(
+                                    text: "${widget.user.details!["poste"]}",
+                                    title: 'Poste actuel :  ',
+                                  ),
+                                  const SizedBox(
+                                    height: 15.0,
+                                  ),
+                                  RichTextLine(
+                                    text: getCompetences(
+                                        widget.user.details!["competences"]),
+                                    title: 'Compétences :  ',
+                                  ),
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    ApplicantInfo(
+                                                      entreprise:
+                                                          widget.user.details![
+                                                              "entreprise"],
+                                                      domaine: widget.user
+                                                          .details!["domaine"],
+                                                      poste: widget.user
+                                                          .details!["poste"],
+                                                      linkedin: widget.user
+                                                          .details!["linkedin"],
+                                                      expYears: widget.user
+                                                          .details!["expYears"]
+                                                          .toString(),
+                                                      competence:
+                                                          widget.user.details![
+                                                              "competences"],
+                                                      url: widget.user.img!,
+                                                    )));
+                                      },
+                                      child: const Icon(
+                                        Icons.edit,
+                                        color: Color(0xff35ddaa),
+                                        size: 40,
+                                      ))
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -157,7 +161,7 @@ class _ApplicantProfileState extends State<ApplicantProfile> {
   String getCompetences(List<dynamic>? list) {
     String cpts = "\n";
     for (String word in list!) {
-      cpts += word + " \n";
+      cpts += word + " \n ";
     }
     return cpts;
   }
